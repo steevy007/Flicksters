acc5dc84b0e59c8008bc1115d6c1ff90b2bd5d6a
Steeve Sanon <steevesanon6@gmail.com>
2017-07-22 15:17:09 -0400

    update

--- app/src/main/java/com/codepath/flicksters/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/codepath/flicksters/adapters/MovieArrayAdapter.java
@@ -2,8 +2,6 @@ package com.codepath.flicksters.adapters;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,39 +22,52 @@ import java.util.ArrayList;
 public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
 
-    public MovieArrayAdapter(Context context, ArrayList<Movie> movies){
-        super(context, android.R.layout.simple_list_item_1,movies);
-
-
+    private static class ViewHolder {
+        ImageView ivMovieImage;
+        TextView tvTitle;
+        TextView tvOverview;
     }
 
-    @NonNull
+    public MovieArrayAdapter(Context context, ArrayList<Movie> movies) {
+        super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
         Movie movie = getItem(position);
-
-        if (convertView==null){
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
+        if (convertView == null) {
+            // If there's no view to re-use, inflate a brand new view for row
+            viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.item_movie,parent,false);
-
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+            viewHolder.ivMovieImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.ivMovieImage);
-        imageView.setImageResource(0);
-
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
-
-        tvTitle.setText(movie.getOriginalTitle());
-        tvOverview.setText(movie.getOverview());
-
+        // Populate the data from the data object via the viewHolder object
+        // into the template view.
+        viewHolder.ivMovieImage.setImageResource(0);
+        viewHolder.tvOverview.setText(movie.getOverview());
+        viewHolder.tvTitle.setText(movie.getOriginalTitle());
+        // Return the completed view to render on screen
         int orientation =  getContext().getResources().getConfiguration().orientation;
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-        Picasso.with(getContext()).load(movie.getPosterpath()).into(imageView);
+            Picasso.with(getContext()).load(movie.getPosterpath()).into(viewHolder.ivMovieImage);
         }
         else if (orientation == Configuration.ORIENTATION_LANDSCAPE){
-            Picasso.with(getContext()).load(movie.getBackdroppath()).into(imageView);
+            Picasso.with(getContext()).load(movie.getBackdroppath()).into(viewHolder.ivMovieImage);
         }
         return convertView;
+
     }
+
 }
